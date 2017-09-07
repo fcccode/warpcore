db37e2d33e9a33a48e0b320afedd0bb0e25c31b5
Travis CI <travis@travis-ci.org>
2017-09-07 08:30:42 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 196.
    
    Commit: 61a907fe5b615723e7544ebf364d56cfb7228ed7.

--- search/all_2.js
+++ search/all_2.js
@@ -11,12 +11,20 @@ var searchData=
   ['backend_5fnetmap_2ec',['backend_netmap.c',['../backend__netmap_8c.html',1,'']]],
   ['backend_5fsock_2ec',['backend_sock.c',['../backend__sock_8c.html',1,'']]],
   ['basename',['basename',['../util_8h.html#abf5f17d3254f2e930aee0dc34dbff071',1,'util.h']]],
+  ['bblk',['BBLK',['../util_8h.html#ab3bdb5557ea3a8f7ed80a55ad301e09c',1,'util.h']]],
+  ['bblu',['BBLU',['../util_8h.html#a9f40b3c1c240d5513da9f50685d5bf15',1,'util.h']]],
+  ['bcyn',['BCYN',['../util_8h.html#abd610b309fe5845f1e5fbd6666a22447',1,'util.h']]],
+  ['bgrn',['BGRN',['../util_8h.html#a003f38f51e5f6972a6a0796bda4ca11c',1,'util.h']]],
   ['bld',['BLD',['../util_8h.html#a6773bd84b2bf2a4f2118fc787f3a53fb',1,'util.h']]],
   ['blk',['BLK',['../util_8h.html#a6283768c836fd4a0f382514973bc3ff4',1,'util.h']]],
   ['bln',['BLN',['../util_8h.html#affc43d19d38fedbf7a83ea1730115c02',1,'util.h']]],
   ['blu',['BLU',['../util_8h.html#add9307de87f38e77d336751e305886f6',1,'util.h']]],
+  ['bmag',['BMAG',['../util_8h.html#af1bdc3f8928aa1589ef44e3578a09464',1,'util.h']]],
+  ['bred',['BRED',['../util_8h.html#a2adb4c9e293ac446897ccfac5a52d6c2',1,'util.h']]],
   ['buf',['buf',['../structw__iov.html#a2b33510e8a5b556deef4fb2b29b62a9e',1,'w_iov']]],
   ['buf_5fidx',['buf_idx',['../structnetmap__slot.html#a53b5fc7beb0712b9c41f344177894ade',1,'netmap_slot']]],
   ['buf_5fofs',['buf_ofs',['../structnetmap__ring.html#a7ffcfff1be1ca50a663914184b7faecf',1,'netmap_ring']]],
-  ['bufs',['bufs',['../structw__engine.html#afa8ad906202b2098b44fb6a5bdeb5c20',1,'w_engine']]]
+  ['bufs',['bufs',['../structw__engine.html#afa8ad906202b2098b44fb6a5bdeb5c20',1,'w_engine']]],
+  ['bwht',['BWHT',['../util_8h.html#ae528cad0e6c76be71eb66776599ec5d9',1,'util.h']]],
+  ['byel',['BYEL',['../util_8h.html#a9b526756196d7c0efa31bc8fe4e5f138',1,'util.h']]]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -6,10 +6,12 @@ var searchData=
   ['dim',['DIM',['../util_8h.html#ac25189db92959bff3c6c2adf4c34b50a',1,'util.h']]],
   ['dir',['dir',['../structnetmap__ring.html#a56efa3d3188e04e44733858f9b0cf6a1',1,'netmap_ring']]],
   ['dlevel',['DLEVEL',['../util_8h.html#a94c2776a2d1f9e44c3d327befe0d3bfe',1,'util.h']]],
-  ['dlock',['DLOCK',['../util_8h.html#a97c15b51609eac87dccc82f73bf3ba39',1,'util.h']]],
-  ['dmaster',['DMASTER',['../util_8h.html#aca1b96b12f6ee95bcc37468756298918',1,'util.h']]],
   ['do_5fregexec',['DO_REGEXEC',['../util_8h.html#a911d17fccac43347bb3c6ace7a8051c8',1,'util.h']]],
   ['dport',['dport',['../structudp__hdr.html#a237bbfa523557d865a0d7d8570ae8afc',1,'udp_hdr']]],
   ['dst',['dst',['../structeth__hdr.html#a62491dbb603459d3b23060cd05944752',1,'eth_hdr::dst()'],['../structip__hdr.html#a8100327fc0ab98d0f4e45a5b461e3706',1,'ip_hdr::dst()']]],
-  ['dunlock',['DUNLOCK',['../util_8h.html#adb362507fe8a1becdede446779b54102',1,'util.h']]]
+  ['dthread_5fgap',['DTHREAD_GAP',['../util_8h.html#ae87fafc79d9317d688dc63fd3d0c805d',1,'util.h']]],
+  ['dthread_5fid',['DTHREAD_ID',['../util_8h.html#a680e4f50570b3400b455076c0cb143bc',1,'util.h']]],
+  ['dthread_5fid_5find',['DTHREAD_ID_IND',['../util_8h.html#ab4e90e0eea07ae337107d6a6fc44f9b8',1,'util.h']]],
+  ['dthread_5flock',['DTHREAD_LOCK',['../util_8h.html#a45e5c88c9ecb59dbebcf010d7c391aa7',1,'util.h']]],
+  ['dthread_5funlock',['DTHREAD_UNLOCK',['../util_8h.html#a6d4b88acd687e9556a203cf43de143a2',1,'util.h']]]
 ];
