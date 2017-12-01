faa6bf3fcd6ffc8351e923ff25c3eb546cd48824
Travis CI <travis@travis-ci.org>
2017-12-01 13:05:05 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 282.
    
    Commit: 052f0d64c9385755f1d4d6475891e0e093001d16.

--- search/defines_c.js
+++ search/defines_c.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['plat_5frandom',['plat_random',['../plat_8h.html#acf8bee0f69b372ea080bb495ecff8109',1,'plat.h']]],
   ['plural',['plural',['../util_8h.html#a0a01a943f8b9a66cdb3221d7e39c8f5d',1,'util.h']]]
 ];
