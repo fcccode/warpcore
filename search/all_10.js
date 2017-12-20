fd4258bf6f0d2fe6376d776ce72a4dae229fce48
Travis CI <travis@travis-ci.org>
2017-12-20 14:35:45 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 301.
    
    Commit: 793465859a77755b8f983ad88bd04614203301bf.

--- search/all_10.js
+++ search/all_10.js
@@ -5,6 +5,6 @@ var searchData=
   ['ring_5fofs',['ring_ofs',['../structnetmap__if.html#a1dfdb80b4e21abc21e01a07b07f37cca',1,'netmap_if']]],
   ['ringid',['ringid',['../structnetmap__ring.html#a9638eb5de920d539aa9a6a518e4d4827',1,'netmap_ring']]],
   ['rip',['rip',['../structw__engine.html#a070cfa61a403b3024f06fbef374097be',1,'w_engine']]],
-  ['rotl',['rotl',['../warpcore_8c.html#a4f05184ca9e937bd3742cd3d02c08e39',1,'warpcore.c']]],
+  ['rotl',['rotl',['../warpcore_8h.html#a4f05184ca9e937bd3742cd3d02c08e39',1,'warpcore.h']]],
   ['rwarn',['rwarn',['../util_8h.html#a7bc0c9794d85e85e8f2d7266c832638f',1,'util.h']]]
 ];
--- search/defines_e.js
+++ search/defines_e.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['recv_5fsize',['RECV_SIZE',['../backend__sock_8c.html#ab05e00e8cea9db60202e746d95818997',1,'backend_sock.c']]],
-  ['rotl',['rotl',['../warpcore_8c.html#a4f05184ca9e937bd3742cd3d02c08e39',1,'warpcore.c']]],
+  ['rotl',['rotl',['../warpcore_8h.html#a4f05184ca9e937bd3742cd3d02c08e39',1,'warpcore.h']]],
   ['rwarn',['rwarn',['../util_8h.html#a7bc0c9794d85e85e8f2d7266c832638f',1,'util.h']]]
 ];
