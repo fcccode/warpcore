8509a02a7d6f4dc8f4966da3d2ca3e7b6b36e5f6
Travis CI <travis@travis-ci.org>
2018-03-02 10:44:47 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 326.
    
    Commit: 0750349bdbe7613f1f006ea3f1de2e6c08faf193.

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['get_5fsock',['get_sock',['../backend_8h.html#aca957d914ea14d5b235523fe733f5c0c',1,'get_sock(struct w_engine *const w, const uint16_t sport, const uint16_t dport):&#160;warpcore.c'],['../warpcore_8c.html#aca957d914ea14d5b235523fe733f5c0c',1,'get_sock(struct w_engine *const w, const uint16_t sport, const uint16_t dport):&#160;warpcore.c']]],
   ['grn',['GRN',['../util_8c.html#aea69ffbacdcdf16c21b8c9961df84448',1,'util.c']]]
 ];
--- search/search.js
+++ search/search.js
@@ -10,7 +10,7 @@ var indexSectionsWithContent =
   0: "_abcdefghilmnopqrstuvwx",
   1: "aeinuw",
   2: "abceinpqrtuw",
-  3: "abcdefginpsuwx",
+  3: "abcdefinpsuwx",
   4: "bcdefhilmnoprstuvw",
   5: "n",
   6: "_abcdeghilmnpqrstuw"
